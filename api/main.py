8f52ea03ceba4b59842520c14cb3d5cfd38faeb4
myogeon-dev <myogeon.dev@gmail.com>
2026-06-23 03:07:05 +0900

    deploy setting

--- api/main.py
+++ api/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import sys
+import os
+
+# 현재 api 폴더의 상위 디렉토리를 시스템 경로에 추가
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app.api.router import api_router
 from app.core.config import settings
 from app.core.cors import get_cors_origin_regex, get_cors_origins
