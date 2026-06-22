5a3245ef8480da7262c314bd48686d901a649c97
myogeon-dev <myogeon.dev@gmail.com>
2026-06-23 04:14:11 +0900

    fix: move app folder inside api for vercel packaging2

--- api/main.py
+++ api/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+import sys
+import os
+
+# 현재 파일(main.py)이 있는 'api' 폴더 자체를 파이썬 경로에 추가
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
 
 from app.api.router import api_router
 from app.core.config import settings
